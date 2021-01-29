4c47d772268cf6ac139ac8ff711bf554de2eebe7
RRRrrra2987 <77225385+RRRrrra2987@users.noreply.github.com>
2021-01-29 14:17:07 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
-var i=0
+var i=0;
+var local=localStorage.getItem("local");
+if (local=0){        
+localStorage.setItem("local", 1)
+}        
+else{
+local=local+1
+localStorage.setItem("local",local)
+};        
 alert("(ﾟ∀ﾟ)ｱﾋｬ");
 function button1()
 {
 i=i+1;
 alert(i+"回クリックしました！");  
-}
+};
 function pcst()
 {
 var PCST=navigator.platform+"\n"
         + navigator.userAgent+"\n";
 alert(PCST);
-}
+};
+function local()
+{
+aleat("あなたがここを訪れたのは、"+local+"回目です！");
+};        
+   
+        
